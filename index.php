4e99f40e8d425590124167cde0528448b862a105
erickkf600 <erickkf600@gmail.com>
2019-02-02 16:45:50 -0200

    theme infoco

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+    <link rel="stylesheet" media="all" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <title>InFoco Publicidades</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
